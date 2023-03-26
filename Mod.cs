73430bdc9ff6cb03de30bfa59307e65d0d6107b4
FloppyDisk <FloppyDisk@none>
2023-03-26 20:20:18 +1300

    Simple mod to double speed

---
+++ Mod.cs
@@ -0,0 +1,14 @@
+﻿using PulsarModLoader;
+
+namespace DoubleSpeed
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.1";
+        public override string Author => "FloppyDisk";
+        public override string Name => "DoubleSpeed";
+        public override string HarmonyIdentifier() => $"{Author}.{Name}";
+        public override string ShortDescription => "Doubles Speed";
+        public override string LongDescription => "Licence: GPL 3.0-only";
+    }
+}
---
+++ SpeedPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace DoubleSpeed
+{
+    [HarmonyPatch(typeof(PLPlayerController), "HandleMovement")]
+    internal class HandleMovement
+    {
+        private static void Prefix(PLPlayerController __instance)
+        {
+            if (__instance.MyPawn && __instance.MyPawn.GetPlayer() && __instance.MyPawn.GetPlayer().GetPlayerID() == PLNetworkManager.Instance.LocalPlayerID)
+            {
+                __instance.DefaultPawnSpeed = 8f;
+                __instance.SprintPawnSpeed = 12f;
+            }
+        }
+    }
+}
